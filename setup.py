7ae008d529f47f4d0d2ab6d813b3e8a033351582
McTavish McArdle <mctavishmcardle@gmail.com>
2020-08-31 19:20:25 -0500

    remove `lib` dir

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import find_packages, setup
 
-src_dir = "lib"
-
 setup(
     name="sccm",
     version="0.1.0",
-    package_dir={"": src_dir},
-    packages=find_packages(where=src_dir),
+    package_dir={"": "."},
+    packages=find_packages(where="."),
     python_requires=">=3.7.*",
     install_requires=["vg>=1.7", "solidpython>=0.4", "numpy>=1.18"],
 )
